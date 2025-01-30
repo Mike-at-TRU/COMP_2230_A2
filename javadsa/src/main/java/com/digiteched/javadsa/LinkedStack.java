a6efde80a91fd5678dbb549b8c0fc0abaf92d104
Aaron Plahn <aplahn85@gmail.com>
2025-01-29 20:58:28 -0800

    set up assignment 2 boilerplate

--- javadsa/src/main/java/com/digiteched/javadsa/LinkedStack.java
+++ javadsa/src/main/java/com/digiteched/javadsa/LinkedStack.java
@@ -3,67 +3,25 @@ package com.digiteched.javadsa;
 import com.digiteched.javadsa.interfaces.IStack;
 
 public class LinkedStack<T> implements IStack<T> {
-    private class LinkedStackNode {
-        public final T data;
-
-        public LinkedStackNode(T data) {
-            this.data = data;
-        }
-
-        private LinkedStackNode next;
-
-        public LinkedStackNode getNext() {
-            return next;
-        }
-
-        public void setNext(LinkedStackNode next) {
-            this.next = next;
-        }
-    }
-
-    private int count;
-
-    private LinkedStackNode top;
-
-    public LinkedStack() {
-        count = 0;
-    }
-
     @Override
     public void push(T item) {
-        LinkedStackNode newNode = new LinkedStackNode(item);
-
-        LinkedStackNode secondInLine = isEmpty() ? null : top;
-
-        top = newNode;
-
-        if (secondInLine != null) {
-            top.setNext(secondInLine);
-        }
-
-        count++;
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public T pop() {
-        // TODO throw if empty
+        throw new UnsupportedOperationException();
 
-        LinkedStackNode outNode = top;
-
-        top = top.getNext();
-
-        count--;
-
-        return outNode.data;
     }
 
     @Override
     public int size() {
-        return count;
+        throw new UnsupportedOperationException();
+
     }
 
     public boolean isEmpty() {
-        return size() == 0;
+        throw new UnsupportedOperationException();
     }
 
 }
